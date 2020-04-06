b435e9cf4c152b339f7055cec52244ca14cec5dd
Manivasagam <manivasaga.mca16@gmail.com>
2020-04-06 10:48:52 +0530

    Adding Loggerfactory

--- src/main/java/com/manivash/SpringBootLoggingApplication.java
+++ src/main/java/com/manivash/SpringBootLoggingApplication.java
@@ -1,13 +1,24 @@
 package com.manivash;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootLoggingApplication {
 
+	static Logger logger = LoggerFactory.getLogger(SpringBootLoggingApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootLoggingApplication.class, args);
+		
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
 	}
 
 }
